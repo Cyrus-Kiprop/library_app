2f7143a9ad8dc4cc0a9ed9f243cf528b23ad7fca
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-04-19 12:05:20 +0300

    configure mongodb

--- app.js
+++ app.js
@@ -11,24 +11,30 @@ const path = require('path');// joins all the strings in the the path
 const app = express();// instance of express
 const port = process.env.PORT || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, './public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap/dist/css/')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/bootstrap/dist/js/')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/jquery/dist/')));
 
 //  ENGINE TEMPLATINGS
 app.set('views', './src/views');
 app.set('view engine', 'ejs'); // set pug || ejs to be the default template engine
 
-const bookRouter = require('./src/routes/bookRoutes'); // imports the bookroutes from the bookRoutes.js module
-
+const nav = [
+  { link: '/books', title: 'Book' }, {
+    link: '/author', title: 'author'
+  }
+];
+const bookRouter = require('./src/routes/bookRoutes')(nav); // imports the bookroutes from the bookRoutes.js module
+const adminRouter = require('./src/routes/adminRouter')(nav);
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 app.get('/books', (req, res) => {
   res.render('index', {
     nav: [{ link: '/books', title: 'Books' }, {
-      link: '/author', title: 'author'
+      link: '/author', title: 'authors'
     }],
     title: 'Library'
   });
